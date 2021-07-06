ecbb1f19d1df919b67d0383afee61613993e18d4
러리 <doralife12@naver.com>
2021-07-07 01:22:40 +0900

    Refactor: Seperate code in runApp()

--- Chapter7/FlutterDDay/lib/main.dart
+++ Chapter7/FlutterDDay/lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutterdday/main_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: '디데이',
-      home: MainPage()
-    )
-  );
+      home: MainPage(),
+    );
+  }
 }
