b56185b0401ddc8d4e18bf6e2a06361c3bb75920
러리 <doralife12@naver.com>
2021-07-07 01:20:02 +0900

    Feat: Seperate code in runApp()

--- Chapter4/step02/lib/main.dart
+++ Chapter4/step02/lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 import 'main_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: '디데이',
       home: MainPage(),
-    )
-  );
+    );
+  }
 }
