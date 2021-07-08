8776c105d29d9610bd9a19156678532ff0c0e9bd
러리 <doralife12@naver.com>
2021-07-09 00:52:50 +0900

    Feat: class example code for chapter 5

---
+++ Chapter5/step01/01.dart
@@ -0,0 +1,26 @@
+class Car { // 이게 클래스에요.
+  int size = 0;
+  String owner = '러리';
+  int velocity = 0; // 속도
+
+  void run() {
+    print('${owner}이(가) 주인인 ${size} 크기의 자동차가 달립니다.');
+    velocity = 100;
+  }
+
+  void stop() {
+    print('${owner}이(가) 주인인 ${size} 크기의 자동차가 멈췄어요.');
+    velocity = 0;
+  }
+}
+
+void main() {
+  Car car = Car();
+  car.run();
+  car.stop();
+
+  car.size = 100;
+  car.owner = '러어리이';
+  car.run();
+  car.stop();
+}
