af2d647498375a3bce64e13d5e3e500c9ff7be76
러리 <doralife12@naver.com>
2021-07-13 01:09:52 +0900

    Feat: example code for list

---
+++ Chapter5/step06/list/list.dart
@@ -0,0 +1,22 @@
+int sum(List<int> list) {
+  // 몰라도 되는 구문이에요! 그냥 리스트의 합을 구해준다 정도만 알면 돼요.
+  return list.reduce((value, element) => value + element);
+}
+
+void main() {
+  List<int> list1 = [1, 4, 2, 3];
+  print(list1[0]); // 1
+  print(list1[1]); // 4
+  print(list1[2]); // 2
+
+  int sum1 = sum(list1);
+  print(sum1); // 1 + 4 + 2 + 3 = 10
+
+  int sum2 = sum([1, 2, 3, 4, 5]);
+  print(sum2); // 1 + 2 + 3 + 4 + 5 = 15
+
+  List<int> list2 = list1.map((value) => value * 2).toList();
+  // 'list1'의 모든 요소에 2를 곱한 리스트를 새로 만들어요.
+  print(list2); // [2, 8, 4, 6]
+  print(sum(list2)); // 2 + 8 + 4 + 6 = 20
+}
