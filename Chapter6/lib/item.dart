d3de14f9d44ed499067a37124c084a3e919d9ae2
러리 <doralife12@naver.com>
2021-07-18 01:17:39 +0900

    Feat: JSON convert

--- Chapter6/lib/item.dart
+++ Chapter6/lib/item.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
 class Item {
   String title;
   DateTime date;
@@ -8,5 +11,22 @@ class Item {
     required this.date,
     this.imagePath
   });
+
+  String toJsonString() {
+    String? imagePath = this.imagePath;
+    String dateString = DateFormat('yyyy-MM-dd').format(date);
+    return jsonEncode({'title': title, 'targetDate': dateString, 'image': imagePath});
+  }
+
+  factory Item.fromJsonString(String jsonString) {
+    return Item.fromJson(jsonDecode(jsonString));
+  }
+
+  factory Item.fromJson(dynamic json) {
+    String title = json['title'];
+    DateTime date = DateFormat('yyyy-MM-dd').parse(json['targetDate']);
+    String? imagePath = json['image'];
+    return Item(title: title, date: date, imagePath: imagePath);
+  }
 }
 
